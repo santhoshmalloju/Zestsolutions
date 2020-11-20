4f464d7e06d8ff90d19b1c5a4893de6e04da10df
santhu <santhuchary143@gmail.com>
2020-11-20 11:08:26 +0530

    Firstcommit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LineChart from './LineChart';
+
+function App() {
+  return (
+    <div className="App">
+      <LineChart/>
+    </div>
+  );
+}
+
+export default App;
